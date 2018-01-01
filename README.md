8f8fb2ea63c0d116c4dc7252d1d3ee9e49f12cfe
ruanshimin <ruanshimin@baidu.com>
2018-01-01 21:21:47 +0800

    hh

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 [![Build Status](https://travis-ci.org/meteorasd555/learn.svg?branch=master)](https://travis-ci.org/meteorasd555/learn)
+[版本图片](https://www.npmjs.com/package/baidu-aip-sdk)
--- test/index.js
+++ test/index.js
@@ -1,9 +1,3 @@
 'use strict';
-let assert = require('assert');
-describe('test require', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(2), -1);
-    });
-  });
-});
+
+return 12;
